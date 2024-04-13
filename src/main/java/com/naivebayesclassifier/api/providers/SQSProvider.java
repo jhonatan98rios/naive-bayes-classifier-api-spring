b73c50733677d780cb4f93ad2cad171877210f7b
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-04-12 22:35:21 -0300

    SQS Provider implementation

--- src/main/java/com/naivebayesclassifier/api/providers/SQSProvider.java
+++ src/main/java/com/naivebayesclassifier/api/providers/SQSProvider.java
@@ -10,9 +10,11 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 
 @Component
-@RequiredArgsConstructor
 public class SQSProvider {
 
     @Value("${aws.numQueueUrl}")
@@ -24,23 +26,32 @@ public class SQSProvider {
     private final QueueMessagingTemplate queueMessagingTemplate;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     public SQSProvider(AmazonSQSAsync amazonSqs) {
         this.queueMessagingTemplate = new QueueMessagingTemplate(amazonSqs);
     }
 
     public boolean sendMessage(final EventPayload messagePayload) {
 
-        Message<EventPayload> msg = MessageBuilder.withPayload(messagePayload)
-                .setHeader("sender", "naive-bayes-classifier-api")
-                .build();
+        try {
+            // Converter o messagePayload em JSON e cria a mensagem
+            String jsonPayload = objectMapper.writeValueAsString(messagePayload);
+            Message<String> msg = MessageBuilder.withPayload(jsonPayload)
+                    .setHeader("sender", "naive-bayes-classifier-api")
+                    .build();
 
-        // Seleciona a fila com base no tipo de Classifier
-        String queueUrl = messagePayload.type().equals("nlp") ? nlpQueueUrl : numQueueUrl;
+            // Seleciona a fila com base no tipo de Classifier
+            String queueUrl = messagePayload.type().equals("nlp") ? nlpQueueUrl : numQueueUrl;
 
-        // Enviar a mensagem para a fila SQS
-        queueMessagingTemplate.send(queueUrl, msg);
+            // Enviar a mensagem para a fila SQS
+            queueMessagingTemplate.send(queueUrl, msg);
+            return true;
 
-        // Retornar verdadeiro se a mensagem foi enviada com sucesso
-        return true;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
